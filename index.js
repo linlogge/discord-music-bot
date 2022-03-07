fdd4d241876eb792f63ec2c16b0ee8b1b69268ad
Nöl <65312133+linlogge@users.noreply.github.com>
2022-03-07 21:00:39 +0100

    add comments

--- index.js
+++ index.js
@@ -10,19 +10,27 @@ const TOKEN = process.env.TOKEN
 
 const LOAD_SLASH = process.argv[2] == "load"
 
+// ID des Bots angeben
 const CLIENT_ID = "824946756110254080"
+// ID des Servers angeben
 const GUILD_ID = "949423992643653692"
 
+// Neuen Discord Client erstellen
 const client = new Discord.Client({
     intents: [
+        // Berechtigung für Zugriff auf die Discord API
         "GUILDS",
+        // Berechtigung für Zugriff auf Voice Chats
         "GUILD_VOICE_STATES"
     ]
 })
 
 client.slashcommands = new Discord.Collection()
+
+// Neuen Musikplayer erstellen
 client.player = new Player(client, {
     ytdlOptions: {
+        // Qualität der Musik abwählen
         quality: "highestaudio",
         highWaterMark: 1 << 25
     }
@@ -30,7 +38,9 @@ client.player = new Player(client, {
 
 let commands = []
 
+// Slash-Commands einlesen
 const slashFiles = fs.readdirSync("./slash").filter(file => file.endsWith(".js"))
+// Slash-Commands einlesen und in commands array speichern
 for (const file of slashFiles){
     const slashcmd = require(`./slash/${file}`)
     client.slashcommands.set(slashcmd.data.name, slashcmd)
@@ -38,6 +48,7 @@ for (const file of slashFiles){
 }
 
 if (LOAD_SLASH) {
+    // Unsere Commands an den Discord Server hochladen
     const rest = new REST({ version: "9" }).setToken(TOKEN)
     console.log("Deploying slash commands")
     rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {body: commands})
@@ -53,20 +64,27 @@ if (LOAD_SLASH) {
     })
 }
 else {
+    // Wenn bot dem Server beitritt, in die Console schreiben
     client.on("ready", () => {
         console.log(`Logged in as ${client.user.tag}`)
     })
+    // Wenn ein Slash-Command in den Chat eingegeben wird, diesen ausführen
     client.on("interactionCreate", (interaction) => {
         async function handleCommand() {
+            // Wenn nachricht kein Slash-Command ist, abbrechen
             if (!interaction.isCommand()) return
 
+            // Slash-Command suchen und ausführen
             const slashcmd = client.slashcommands.get(interaction.commandName)
+            // Wenn Slash-Command nicht gefunden wurde, abbrechen
             if (!slashcmd) interaction.reply("Not a valid slash command")
 
             await interaction.deferReply()
+            // Slash-Command ausführen (Run methode, CLient = Bot, Interaction = Nachricht)
             await slashcmd.run({ client, interaction })
         }
         handleCommand()
     })
     client.login(TOKEN)
 }
+
